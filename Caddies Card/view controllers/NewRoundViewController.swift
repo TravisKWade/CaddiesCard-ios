d264643c38a0d75c4e98c3c3ea755aa85a9f64cb
Travis Wade <travis@entrision.com>
2020-01-09 14:36:21 -0800

    delete player

--- Caddies Card/view controllers/NewRoundViewController.swift
+++ Caddies Card/view controllers/NewRoundViewController.swift
@@ -61,4 +61,11 @@ class NewRoundViewController: UIViewController, UITableViewDelegate, UITableView
 
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            players.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } 
+    }
 }
